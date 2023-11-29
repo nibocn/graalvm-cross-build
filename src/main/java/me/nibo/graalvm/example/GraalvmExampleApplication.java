86d372435279e33d39202b8292ce6ef5e76466d6
NiBo <nibocn@gmail.com>
2023-11-29 22:08:13 +0800

    test: Spring Boot

---
+++ src/main/java/me/nibo/graalvm/example/GraalvmExampleApplication.java
@@ -0,0 +1,13 @@
+package me.nibo.graalvm.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraalvmExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraalvmExampleApplication.class, args);
+	}
+
+}
